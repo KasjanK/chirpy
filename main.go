8b81c5a90ba6b4f064fb7a0d12a53baaa0b2477a
KasjanK <kowalczykkasjan@gmail.com>
2025-11-14 03:29:20 +0100

    feat: show how many times a page is viewed

--- main.go
+++ main.go
@@ -3,12 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerReqCount(w http.ResponseWriter, r *http.Request) {
+	reqCount := &cfg.fileserverHits
+	w.Write([]byte("Hits: " + strconv.Itoa(int(reqCount.Load()))))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)	
 }
 
 func main() {
@@ -18,7 +42,11 @@ func main() {
 		Addr: ":8080",
 	}
 
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiCfg := &apiConfig{}
+
+	serveMux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("/metrics", apiCfg.handlerReqCount)
 	serveMux.HandleFunc("/healthz", handlerReadiness)
+	serveMux.HandleFunc("/reset", apiCfg.handlerReset)
 	log.Fatal(server.ListenAndServe())
 }
