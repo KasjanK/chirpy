69dd7cde807cce5eb1775b87a25d761e50d8d0df
KasjanK <kowalczykkasjan@gmail.com>
2025-11-18 17:33:10 +0100

    refactoring

---
+++ handler_validate.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"slices"
+	"strings"
+)
+
+func (cfg *apiConfig) handlerValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body 	string `json:"body"`
+	}
+
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not decode parameters", err)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+
+	cleanBody := replaceProfanities(params.Body)
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleanBody,
+	})
+}
+
+func replaceProfanities(text string) string {
+	profanities := []string{"kerfuffle", "sharbert", "fornax"}
+	words := strings.Split(text, " ")
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+		if slices.Contains(profanities, lowerWord) {
+			words[i] = "****"	
+		}	
+	}
+	return strings.Join(words, " ")
+}
--- main.go
+++ main.go
@@ -1,116 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"slices"
-	"strings"
 	"sync/atomic"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
-}
-
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) handlerReqCount(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	reqCount := &cfg.fileserverHits
-	w.Write([]byte(fmt.Sprintf(`
-	<html>
-		<body>
-			<h1>Welcome, Chirpy Admin</h1>
-			<p>Chirpy has been visited %d times!</p>
-		</body>
-	</html>
-	`, reqCount.Load())))
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)	
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
-	if err != nil {
-		log.Println(err)
-	}
-
-	type errorResp struct {
-		Error string
-	}
-	respondWithJSON(w, code, errorResp{
-		Error: msg,
-	})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(dat)
-}
-
-func (cfg *apiConfig) handlerValidate(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body 	string `json:"body"`
-	}
-
-	type returnVals struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could not decode parameters", err)
-		return
-	}
-
-	if len(params.Body) > 140 {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
-		return
-	}
-
-	cleanBody := replaceProfanities(params.Body)
-	respondWithJSON(w, http.StatusOK, returnVals{
-		CleanedBody: cleanBody,
-	})
-}
-
-func replaceProfanities(text string) string {
-	profanities := []string{"kerfuffle", "sharbert", "fornax"}
-	words := strings.Split(text, " ")
-	for i, word := range words {
-		lowerWord := strings.ToLower(word)
-		if slices.Contains(profanities, lowerWord) {
-			words[i] = "****"	
-		}	
-	}
-	return strings.Join(words, " ")
 }
 
 func main() {
@@ -129,7 +26,6 @@ func main() {
 	serveMux.HandleFunc("GET /api/healthz", handlerReadiness)
 	serveMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	serveMux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidate)
-
 
 	log.Fatal(server.ListenAndServe())
 }
